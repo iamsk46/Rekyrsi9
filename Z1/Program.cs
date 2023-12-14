6748881210f4d6fe16b0e413be7724023fa81cb4
iamsk46 <lomalstekloy@gmail.com>
2023-12-14 12:11:15 +0300

    Z1Ready

---
+++ Z1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы. ");
+string firstText = "Введите значение М => ";
+System.Console.Write(firstText);
+int firstNumber = NumberInTerminal(0xFFFFFF,firstText,1);
+string secondText ="Введите значение N => ";
+System.Console.Write(secondText);
+int secondNumber = NumberInTerminal(0xFFFFFF,secondText,1);
+System.Console.WriteLine($"Числа от {firstNumber} до {secondNumber} => {ShowDigit(firstNumber,secondNumber)}");
+static string ShowDigit(int firstDigit,int secondDigit)
+    {
+        if (firstDigit > secondDigit)
+        {
+            return "";
+        }
+        return $"{firstDigit} " + ShowDigit(firstDigit+1,secondDigit);
+    }
+        static int NumberInTerminal(int numberDigits, string repeatString, int minValueSet)
+        {
+            string ? numString = Console.ReadLine();
+            int numInt = 0;
+            while ((!Int32.TryParse(numString,out numInt)) 
+                    || !(numInt >= minValueSet) 
+                    || !(numInt <= numberDigits)
+                  )
+            {
+                System.Console.WriteLine("Ошибка ввода, повторите");
+                System.Console.Write(repeatString);
+                numString = Console.ReadLine(); 
+            }
+            return numInt;
+        }
